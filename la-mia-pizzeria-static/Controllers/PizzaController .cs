f1a3c7f8e93dedc5af3c2e94a427d0e36f1c25dd
Luigi9411 <luigi.sciavicco@gmail.com>
2023-04-08 21:57:04 +0200

    detail

--- la-mia-pizzeria-static/Controllers/PizzaController .cs
+++ la-mia-pizzeria-static/Controllers/PizzaController .cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -6,7 +7,21 @@ namespace la_mia_pizzeria_static.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            using var ctx = new PizzaContext();
+
+            var pizzas = ctx.Pizzas.ToArray();
+
+            return View(pizzas);
+        }
+
+        public IActionResult Detail(int id)
+        {
+
+            using var ctx = new PizzaContext();
+
+            var pizza = ctx.Pizzas.Find(id);
+
+            return View(pizza);
         }
     }
 }
